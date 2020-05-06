dbb00d0024bcf61390ad93e6f26fb9729c7a8187
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-05-07 01:09:48 +0900

    Add：コントローラーのGetメソッドで呼び出すFindByTokenメソッドを作成
    クエリを実行して取得したカラムを格納して返す

---
+++ database/user_repository.go
@@ -0,0 +1,35 @@
+package database
+
+import "techtrain-CA/models"
+
+type UserRepository struct {
+  SqlHandler *SqlHandler
+}
+
+func (repo *UserRepository) FindByToken(identifier int) (user models.User, err error) {
+  row, err := repo.SqlHandler.Query("SELECT id, name, created_at, updated_at FROM users WHERE id = ?", identifier)
+
+  defer row.Close()
+
+  if err != nil {
+    return
+  }
+
+  var id int
+  var name string
+  var created_at string
+  var updated_at string
+
+  row.Next()
+
+  if err = row.Scan(&id, &name, &created_at, &updated_at); err != nil {
+    return
+  }
+
+  user.Id = id
+  user.Name = name
+  user.CreatedAt = created_at
+  user.UpdatedAt = updated_at
+
+  return
+}
