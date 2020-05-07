cfc563b02b245f3b4e0b1224325f127ff3a486fb
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-05-07 21:49:30 +0900

    Add：保存するためのクエリを実行して保存したユーザーのidを返す

--- database/user_repository.go
+++ database/user_repository.go
@@ -7,6 +7,25 @@ type UserRepository struct {
   SqlHandler *SqlHandler
 }
 
+// 保存して保存した行のidを返す
+func (repo *UserRepository) Store(user models.User) (id int, err error) {
+  result, err := repo.SqlHandler.Execute("INSERT INTO users (name) VALUES (?)", user.Name,)
+
+  if err != nil {
+    return
+  }
+
+  // 保存した行のidを取得
+  identifier, err := result.LastInsertId()
+  if err != nil {
+    return
+  }
+
+  // intに変換
+  id = int(identifier)
+  return
+}
+
 // クエリを実行して結果を返す
 func (repo *UserRepository) FindByToken(identifier int) (user models.User, err error) {
   row, err := repo.SqlHandler.Query("SELECT id, name, created_at, updated_at FROM users WHERE id = ?", identifier)
