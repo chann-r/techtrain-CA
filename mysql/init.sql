01c10de1bf817b0a5296a21bbfeb1466c50c0731
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-05-12 23:38:47 +0900

    Add：初期データを修正

--- mysql/init.sql
+++ mysql/init.sql
@@ -5,9 +5,10 @@ USE dojo_db;
 CREATE TABLE users(
   id              INTEGER PRIMARY KEY AUTO_INCREMENT,
   name            VARCHAR(40) NOT NULL,
+  token           VARCHAR(255),
   created_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-INSERT INTO users (name) VALUES ("林 遼太朗");
-INSERT INTO users (name) VALUES ("John Titor");
+INSERT INTO users (name, token) VALUES ("林 遼太朗", "11111");
+INSERT INTO users (name, token) VALUES ("John Titor", "22222");
