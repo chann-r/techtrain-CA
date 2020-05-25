23be5216a5376c3951f47d0a8bd2c2f57307ee5f
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-05-25 18:29:56 +0900

    Add：ガチャを引くためのエンドポイントとコントローラーを追加

--- src/controllers/router.go
+++ src/controllers/router.go
@@ -15,12 +15,15 @@ func init() {
 
   // DBに接続 & コントローラーを初期化
   userController := NewUserController(database.NewSqlHandler())
+  gachaController := NewGachaController(database.NewSqlHandler())
 
   //ユーザー関連のエンドポイント
   router.POST("/user/create", func(c *gin.Context) { userController.Create(c) })
   router.GET("/user/get/:id", func(c *gin.Context) { userController.GetUser(c) })
   router.GET("/user/get", func(c *gin.Context) { userController.Get(c) })
   router.PUT("/user/update", func(c *gin.Context) { userController.Update(c) })
+
+  router.POST("/gacha/draw", func(c *gin.Context) { gachaController.Draw(c) })
 
   Router = router
 }
