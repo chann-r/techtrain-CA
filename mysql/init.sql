ce07e40cbc3210bf99404bf93361b66585799ed2
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-05-25 18:01:06 +0900

    Add：キャラクターとコレクションのためのテーブル作成

--- mysql/init.sql
+++ mysql/init.sql
@@ -10,5 +10,34 @@ CREATE TABLE users(
   updated_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
+CREATE TABLE characters(
+  id   INTEGER PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE collections(
+  id              INTEGER PRIMARY KEY AUTO_INCREMENT,
+  user_id         INTEGER,
+  character_id    INTEGER,
+
+  CONSTRAINT fk_user_id
+    FOREIGN KEY (user_id)
+    REFERENCES users (id)
+    ON DELETE SET NULL ON UPDATE CASCADE,
+
+  CONSTRAINT fk_character_id
+    FOREIGN KEY (character_id)
+    REFERENCES characters (id)
+    ON DELETE SET NULL ON UPDATE CASCADE
+);
+
 INSERT INTO users (name, token) VALUES ("林 遼太朗", "11111");
 INSERT INTO users (name, token) VALUES ("John Titor", "22222");
+
+INSERT INTO characters (name) VALUES ("フシギダネ");
+INSERT INTO characters (name) VALUES ("ヒトカゲ");
+INSERT INTO characters (name) VALUES ("ゼニガメ");
+
+INSERT INTO collections (user_id, character_id) VALUES (1, 1);
+INSERT INTO collections (user_id, character_id) VALUES (1, 2);
+INSERT INTO collections (user_id, character_id) VALUES (2, 3);
