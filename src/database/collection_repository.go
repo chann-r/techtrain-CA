182b21623da4f1b6df77924ef138b12d7b99c48f
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-05-27 23:04:34 +0900

    Fix：ガチャの内容を保存する際に、返すのをintからintのスライスに変更

--- src/database/collection_repository.go
+++ src/database/collection_repository.go
@@ -10,26 +10,33 @@ type CollectionRepository struct {
   SqlHandler *SqlHandler
 }
 
-// ランダムにcharacter_idを生成してuser_idと一緒に保存する関数
-func (repo *CollectionRepository) Store(user_id int) (id int, err error) {
+// timesの数だけランダムにcharacter_idを生成してuser_idと一緒に保存する関数
+func (repo *CollectionRepository) Store(user_id int, times int) (characterIds []int, err error) {
   // シードを与える（デフォルトだと同じ乱数ジェネレーターを使用してしまう）
   rand.Seed(time.Now().UnixNano())
-  // 0から2までの乱数に1を足す
-  character_id := rand.Intn(2) + 1
 
-  result, err := repo.SqlHandler.Execute("INSERT INTO collections (user_id, character_id) VALUES (?, ?)", user_id, character_id)
-  if err != nil {
-    return
+  for i := 1; i <= times; i++ {
+    // 0から2までの乱数に1を足す
+    character_id := rand.Intn(2) + 1
+
+    result, _ := repo.SqlHandler.Execute("INSERT INTO collections (user_id, character_id) VALUES (?, ?)", user_id, character_id)
+    if err != nil {
+      return
+    }
+
+    // 保存した行のidを取得
+    identifier, _ := result.LastInsertId()
+    if err != nil {
+      return
+    }
+
+    // intに変換
+    id := int(identifier)
+
+    // スライスの要素に保存したcollectionのideを追加
+    characterIds = append(characterIds, id)
   }
 
-  // 保存した行のidを取得
-  identifier, err := result.LastInsertId()
-  if err != nil {
-    return
-  }
-
-  // intに変換
-  id = int(identifier)
   return
 }
 
