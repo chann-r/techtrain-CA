bfeb31e3a92d66cccb3082559014a527ce5067cf
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-05-27 20:03:15 +0900

    Add：データベースからトークンを照会して処理できるように、UserRepositoryをGachaControllerのフィールドに追加

--- src/controllers/gacha_controller.go
+++ src/controllers/gacha_controller.go
@@ -7,6 +7,7 @@ import (
 
 type GachaController struct {
   CollectionRepository database.CollectionRepository
+  UserRepository       database.UserRepository
 }
 
 func NewGachaController(sqlHandler *database.SqlHandler) *GachaController {
@@ -14,9 +15,26 @@ func NewGachaController(sqlHandler *database.SqlHandler) *GachaController {
     CollectionRepository: database.CollectionRepository{
       SqlHandler: sqlHandler,
     },
+    UserRepository: database.UserRepository{
+      SqlHandler: sqlHandler,
+    },
   }
 }
 
 func (controller *GachaController) Draw(c *gin.Context) {
-  c.JSON(200, "a")
+  // ヘッdサーのtokenを取得
+  tokenString := c.Request.Header.Get("x-token")
+  if tokenString == "" {
+    c.JSON(500, "token must be needed.")
+    return
+  }
+
+  // トークンでユーザーを検索
+  user, err := controller.UserRepository.FindByToken(tokenString)
+  if err != nil {
+		c.JSON(500, err.Error())
+		return
+	}
+
+  c.JSON(200, user)
 }
